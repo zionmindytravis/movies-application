d3b9e4a79df89be7c141f113101322e88dc4b934
mjtillman <mjtillman@gmail.com>
2019-03-15 09:44:22 -0500

    Added functionality to add movies to database

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const {postMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   // console.log('Here are all the movies:');
@@ -35,19 +36,20 @@ const getHTML = function(title, rating, id) {
 const addMovie = () =>
     $('#submit').on('click', function() {
 
-      const newMovie = {
-          title: $('#title').val(),
-          rating: $('input[name = rating]:checked').val(),
-          // id: something
-      };
-      const url = 'api/movies';
-      const options = {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newMovie),
-      }
+        const newMovie = {
+            title: $('#title').val(),
+            rating: $('input[name = rating]:checked').val(),
+        };
 
+        postMovies(newMovie);
+        getMovies().then((movies) => {
+            // console.log('Here are all the movies:');
+            movies.forEach(({title, rating, id}) => {
+
+                $('#movies').append(getHTML(title, rating, id));
+                // console.log(`id#${id} - ${title} - rating: ${rating}`);
+            });
+        });
     });
+
 addMovie();
